8391a9d8047a2690cf40097fdd0bdd6db7b97144
LeTatTuan <letattuan04052003@gmail.com>
2024-01-18 23:15:11 +0700

    user ajax to call api Login

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,15 +1,7 @@
 
 
 const getLoginPage = (req, res) => {
-    const arrMessage = req.flash('data');
-    const error = arrMessage[0] ? arrMessage[0] : '';
-    const inputEmail = arrMessage[1] ? arrMessage[1] : '';
-    const EC = arrMessage[2] ? arrMessage[2] : '';
-    return res.render('login.ejs', {
-        error,
-        inputEmail,
-        EC,
-    });
+    return res.render('login.ejs')
 }
 
 module.exports = {
